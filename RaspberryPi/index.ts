0014250c5de8a25672be44e2cdf06bf78eff1414
Andy Sturrock <github@sturrock.org>
2022-05-15 22:16:26 +0100

    Use AWS IOT to send status messages when turning relays on and off.

--- RaspberryPi/index.ts
+++ RaspberryPi/index.ts
@@ -1,12 +1,38 @@
 require('source-map-support').install();
 const CronJob = require('cron').CronJob;
 
+import dotenv from "dotenv";
+dotenv.config({debug: true});
+
+const awscrt = require('aws-crt');
+const iot = awscrt.iot;
+const mqtt = awscrt.mqtt;
+
+const certFile = process.env.CERTFILE;
+const keyFile = process.env.KEYFILE;
+const caFile = process.env.CAFILE;
+const config_builder = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(certFile, keyFile);
+config_builder.with_certificate_authority_from_path(undefined, caFile);
+config_builder.with_clean_session(false);
+// config_builder.with_client_id("test-" + Math.floor(Math.random() * 100000000));
+const clientId = process.env.CLIENTID;
+config_builder.with_client_id(clientId);
+const endpoint = process.env.ENDPOINT;
+config_builder.with_endpoint(endpoint);
+const config = config_builder.build();
+
+const client = new mqtt.MqttClient();
+const connection = client.new_connection(config);
+const topic = process.env.TOPIC;
+
 import { Relay } from "./relay";
 // Global so we can access in the SIGINT handler
 const relay1 = new Relay(Relay.RELAY1);
 const relay2 = new Relay(Relay.RELAY2);
 const relay3 = new Relay(Relay.RELAY3);
 const relay4 = new Relay(Relay.RELAY4);
+
+let sequence = 1;
 
 
 // Turn off all the relays if we are being killed by SIGINT
@@ -15,7 +41,17 @@ process.on('SIGINT', async () => {
   await relay2.off();
   await relay3.off();
   await relay4.off();
-  console.log('Caught SIGINT, turned relays off.');
+  console.log('Caught SIGINT, turned relays off and disconnecting from AWS.');
+  const message = `GardenIOT going down...`;
+  console.log(message);
+  const msg = {
+    message: message,
+    sequence: sequence++,
+  };
+  const json = JSON.stringify(msg);
+  const res = await connection.publish(topic, json, mqtt.QoS.AtLeastOnce);
+  console.log(`res = ${JSON.stringify(res)}`);
+  await connection.disconnect();
   // Shouldn't be necessary, but just in case, pause to give relays time to switch off.
   await sleep(500);
   process.exit();
@@ -25,14 +61,30 @@ async function sleep(millis : number) {
   return new Promise(resolve => setTimeout(resolve, millis));
 }
 
-function relayOn(relay: Relay) {
+async function relayOn(relay: Relay) {
   relay.on();
-  console.log(`Turning on relay ${relay.name()} (pin ${relay.id()})...`);
+  const message = `Turning on relay ${relay.name()} (pin ${relay.id()})...`;
+  console.log(message);
+  const msg = {
+    message: message,
+    sequence: sequence++,
+  };
+  const json = JSON.stringify(msg);
+  const res = await connection.publish(topic, json, mqtt.QoS.AtLeastOnce);
+  console.log(`res = ${JSON.stringify(res)}`);
 }
 
-function relayOff(relay: Relay) {
+async function relayOff(relay: Relay) {
   relay.off();
-  console.log(`Turning off relay ${relay.name()} (pin ${relay.id()})...`);
+  const message = `Turning off relay ${relay.name()} (pin ${relay.id()})...`;
+  console.log(message);
+  const msg = {
+    message: message,
+    sequence: sequence++,
+  };
+  const json = JSON.stringify(msg);
+  const res = await connection.publish('test/topic', json, mqtt.QoS.AtLeastOnce);
+  console.log(`res = ${JSON.stringify(res)}`);
 }
 
 async function main() {
@@ -55,6 +107,8 @@ async function main() {
   )
 
   offJob.start();
+
+  await connection.connect();
 }
 
 main();
