7aee93e209dd9f001d9c6c6839eb699bee71db41
Andy Sturrock <github@sturrock.org>
2022-06-19 17:48:30 -0400

    Add mock gpio module for testing on non-rpi devices.

---
+++ RaspberryPi/__tests__/mock-gpio.ts
@@ -0,0 +1,15 @@
+class MockGPIO {
+
+    static async setup(channel : number, value : any) {
+
+    }
+
+    static DIR_OUT = 1;
+
+    static async write(channel : number, value : any) {
+
+    }
+
+}
+
+export { MockGPIO }
