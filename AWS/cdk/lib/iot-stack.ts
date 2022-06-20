3f5ad9e01404b4340fdaa2577d2e24993ddc7e90
Andy Sturrock <github@sturrock.org>
2022-06-20 23:18:02 +0100

    Add creation of thing and policy to cdk for IOT stack.

--- AWS/cdk/lib/iot-stack.ts
+++ AWS/cdk/lib/iot-stack.ts
@@ -1,7 +1,8 @@
 import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as logs from 'aws-cdk-lib/aws-logs';
-import * as iot from '@aws-cdk/aws-iot-alpha';
+import * as iot from 'aws-cdk-lib/aws-iot';
+import * as iot_alpha from '@aws-cdk/aws-iot-alpha';
 import * as actions from '@aws-cdk/aws-iot-actions-alpha';
 import { getEnv } from './common';
 
@@ -10,14 +11,54 @@ export class IOTStack extends Stack {
     super(scope, id, props);
 
     const loggingTopic = getEnv('LOGGING_TOPIC', false)!;
+    const clientId = getEnv('CLIENTID', false)!;
 
     const logGroup = new logs.LogGroup(this, 'StatusGroup');
 
-    new iot.TopicRule(this, 'LoggingTopicRule', {
+    new iot_alpha.TopicRule(this, 'LoggingTopicRule', {
       topicRuleName: 'LoggingTopicRule', // optional
       description: 'Saves messages from the logging topic to S3', // optional
-      sql: iot.IotSql.fromStringAsVer20160323(`SELECT * FROM '${loggingTopic}'`),
+      sql: iot_alpha.IotSql.fromStringAsVer20160323(`SELECT * FROM '${loggingTopic}'`),
       actions: [new actions.CloudWatchLogsAction(logGroup)],
+    });
+
+    const cfnThing = new iot.CfnThing(this, 'CfnThing', {
+      thingName: `${clientId}`,
+    });
+
+    const policyDocument = {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "iot:Publish",
+            "iot:Receive",
+            "iot:RetainPublish"
+          ],
+          "Resource": [
+            `arn:aws:iot:${this.region}:${this.account}:topic/${loggingTopic}`
+          ]
+        },
+        {
+          "Effect": "Allow",
+          "Action": "iot:Subscribe",
+          "Resource": [
+            `arn:aws:iot:${this.region}:${this.account}:topicfilter/${loggingTopic}`
+          ]
+        },
+        {
+          "Effect": "Allow",
+          "Action": "iot:Connect",
+          "Resource": [
+            `arn:aws:iot:${this.region}:${this.account}:client/${clientId}`
+          ]
+        }
+      ]
+    }
+    const cfnPolicy = new iot.CfnPolicy(this, 'MyCfnPolicy', {
+      policyDocument: policyDocument,
+      policyName: 'GardenIOTPolicy',
     });
   }
 }
